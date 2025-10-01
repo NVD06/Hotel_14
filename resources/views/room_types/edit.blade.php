e79007e304ebd0dc62ff7527d7790b70e3f9432d
NVD06 <concoko35@gmail.com>
2025-10-01 23:28:31 +0700

    backup before UI reset

--- resources/views/room_types/edit.blade.php
+++
@@ -1,4 +0,0 @@
-<form method="POST" action="{{ route('room-types.update',$roomType) }}" class="space-y-4">
-  @csrf @method('PUT')
-  <!-- input + value="{{ old('name',$roomType->name) }}" ... -->
-</form>
